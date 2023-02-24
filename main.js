e5f1343376902b624f868e91a4d7e1ab31c7fd67
Sxnty <sxntycsgo@gmail.com>
2023-02-24 16:50:49 -0300

    first module

---
+++ main.js
@@ -0,0 +1,5 @@
+const math = require('./math/index')
+console.log(math.add(2, 4))
+console.log(math.substract(2, 4))
+console.log(math.multiply(2, 4))
+console.log(math.divide(2, 4))
---
+++ math/index.js
@@ -0,0 +1,22 @@
+const add = (x, y) => {
+  return x + y;
+};
+
+const substract = (x, y) => {
+  return x - y;
+};
+
+const multiply = (x, y) => {
+  return x * y;
+};
+
+const divide = (x, y) => {
+  return x / y;
+};
+
+module.exports = {
+    add,
+    substract,
+    multiply,
+    divide
+}
