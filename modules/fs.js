bfbff7154877e95dca9c8fe4538e63a461d4f58a
Sxnty <sxntycsgo@gmail.com>
2023-02-24 21:03:43 -0300

    async fs module

--- modules/fs.js
+++ modules/fs.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
-const first = fs.readFileSync("../data/first.txt", "utf-8");
+/*                 Sync                 */
+
+/* const first = fs.readFileSync("../data/first.txt", "utf-8");
 const second = fs.readFileSync("../data/second.txt").toString();
 
 console.log(first);
@@ -9,4 +11,21 @@ console.log(second);
 const title = "contenido del archivo";
 fs.writeFileSync("../data/third.txt", title, {
   flag: "a",
+});
+ */
+
+/*                     Async                    */
+
+fs.readFile("../data/first.txt", "utf-8", (error, data) => {
+  if (error) console.log(error);
+  console.log(data);
+  fs.readFile("../data/second.txt", "utf-8", (error, data) => {
+    if (error) console.log(error);
+    console.log(data);
+
+    fs.writeFile("../data/newFile.txt", "archivo creado desde fs", (error, data) => {
+      console.log(error)
+      console.log(data)
+    });
+  });
 });
