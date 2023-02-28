611fdd832a8a3c8f0c685c236961a5c90aa6d5c3
Sxnty <sxntycsgo@gmail.com>
2023-02-28 12:20:00 -0300

    events

---
+++ modules/events.js
@@ -0,0 +1,11 @@
+const EventEmitter = require("events");
+
+const customEmitter = new EventEmitter();
+customEmitter.on("response", (data, secondData) => {
+  console.log(data);
+  console.log(secondData);
+});
+
+customEmitter.emit("response", "hello world", '123');
+customEmitter.emit("response", {name:'jose'}, '321');
+
